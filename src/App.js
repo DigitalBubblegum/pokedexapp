df8fce79bce785d3a8f819188a3b0fc60797eb63
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-08-12 20:02:33 +0530

    -some functionality achieved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,13 @@
+import axios from "axios";
+var url = "https://pokeapi.co/api/v2/pokemon";
 
+const handleClick = () =>{
+  const promise = axios.get(`${url}/1`)
+  console.log(promise);
+  const promise2 = axios.get(`${url}/2`)
+  console.log(promise2)
+  console.log('button clicked')
+}
 
 function App() {
   return (
@@ -7,8 +16,12 @@ function App() {
         <img src="pokemon_image.png" alt="Pokemon" />
       </div>
       <div className="controls">
-        <button className="prev-btn">Previous</button>
-        <button className="next-btn">Next</button>
+        <button className="prev-btn" onClick={() => handleClick()}>
+          Previous
+        </button>
+        <button className="next-btn" onClick={() => console.log("clk")}>
+          Next
+        </button>
       </div>
     </div>
   );
