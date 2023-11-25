1fdca83aaf66e0c37e1b4050c56913c70ba000c1
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-11-25 16:12:02 +0530

    -css work

--- src/components/PokeInformation.jsx
+++ src/components/PokeInformation.jsx
@@ -3,15 +3,15 @@ const PokeInformation = ({pokemonSingle}) => {
         <div>
             { pokemonSingle === null ? <p className="defaultPokemonInformation">enter a pokemon to get started</p> : 
             <>
-            <h1>{pokemonSingle.name.toUpperCase()}</h1>
+            <h1 className="pokemon-name">{pokemonSingle.name.toUpperCase()}</h1>
             <div className="dexViewMain">
                 <img src={pokemonSingle.sprites.other.home.front_default} alt={pokemonSingle.name} className="pokemon-image"></img>
                 <div>
                     <h2>About</h2>
                     <div className="about-iwh">
-                        ID: {pokemonSingle.id}
-                        Height: {pokemonSingle.height}
-                        Weight: {pokemonSingle.weight}
+                        <h3>ID: {pokemonSingle.id}</h3>
+                        <h3>Height: {pokemonSingle.height}</h3>
+                        <h3>Weight: {pokemonSingle.weight}</h3>
                     </div>
                     <h3>Types</h3>
                     <ul>
