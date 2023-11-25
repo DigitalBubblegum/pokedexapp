7b3d03a36644aa11183375094ea06dba375ff9be
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-11-25 15:45:05 +0530

    -small changes to css

--- src/components/PokeInformation.jsx
+++ src/components/PokeInformation.jsx
@@ -8,13 +8,15 @@ const PokeInformation = ({pokemonSingle}) => {
                 <img src={pokemonSingle.sprites.other.home.front_default} alt={pokemonSingle.name}></img>
                 <div>
                     <h2>About</h2>
+                    <div className="about-iwh">
+                        <h3>ID: {pokemonSingle.id}</h3>
+                        <h3>Height: {pokemonSingle.height}</h3>
+                        <h3>Weight: {pokemonSingle.weight}</h3>
+                    </div>
                     <h3>Types</h3>
                     <ul>
                         {pokemonSingle.types.map(type => <li key={type.slot}>{type.type.name}</li>)}
                     </ul>
-                    <h3>ID: {pokemonSingle.id}</h3>
-                    <h3>Height: {pokemonSingle.height}</h3>
-                    <h3>Weight: {pokemonSingle.weight}</h3>
                     <h2>Stats</h2>
                     <ul>
                         {pokemonSingle.stats.map(stat => <li key={stat.stats}>{stat.stat.name} : {stat.base_stat}</li>)}
--- src/index.js
+++ src/index.js
@@ -4,9 +4,9 @@ import App from './App'
 import { Provider } from 'react-redux'
 import store from './reducers/store'
 import './index.css'
-const logger = require('./utils/logger')
+// const logger = require('./utils/logger')
 const root = ReactDOM.createRoot(document.getElementById('root'))
-logger.info(store.getState())
+// logger.info(store.getState())
 root.render(
   <Provider store={store}>
     <App />
