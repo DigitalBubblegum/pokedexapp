26d8b2b9d5e0b9f89969f57363a9039e65ba96e0
DigitalBubblegum <ghoshaldiwakar@gmail.com>
2023-11-27 19:14:10 +0530

    -rebuild

--- src/services/fetchPokemon.jsx
+++ src/services/fetchPokemon.jsx
@@ -19,4 +19,5 @@ const findSinglePokemon = async(searchVal) => {
     }
 }
 
-export default {getAllPokemon,findSinglePokemon}
+// eslint-disable-next-line import/no-anonymous-default-export
+export default { getAllPokemon,findSinglePokemon }
